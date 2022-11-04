64456145515e43664095819831a135be210f5770
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-04 10:00:01 -0700

    Test code in devTools.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,16 @@ function Pizza() {
   this.size = null;
 }
 
-Pizza.prototype.calculateCost = function() {
+Pizza.prototype.calculateCost = function(pizza) {
   let basePrice = 10;
   let perTopping = 4;
-  let medium = 6;
-  let large = 10;
   let cost = basePrice + (Object.keys(this.toppings).length * perTopping) + this.size;
-  
-  
+  if (this.size === "medium") {
+    cost += 6;
+  }
+  else if (this.size === "large") {
+    cost += 10;
+  }
+  console.log(cost);
+  return cost;
 }
