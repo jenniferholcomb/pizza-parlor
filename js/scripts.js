fa1d0d32e3e4fef2acc853b79a423282b44333ce
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-04 10:15:48 -0700

    Start building index.html with form for user input.

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,18 @@ function Pizza() {
   this.size = null;
 }
 
-Pizza.prototype.calculateCost = function(pizza) {
+Pizza.prototype.calculateCost = function() {
   let basePrice = 10;
   let perTopping = 4;
   let cost = basePrice + (Object.keys(this.toppings).length * perTopping) + this.size;
+  console.log(cost);
   if (this.size === "medium") {
     cost += 6;
   }
   else if (this.size === "large") {
     cost += 10;
   }
-  console.log(cost);
   return cost;
 }
+
+// UI Logic
